8f0c910e24543e67d658c2cc0823551dc02de6b1
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-11-12 13:02:33 +0300

    Added documenting comments.

--- src/Axessors.php
+++ src/Axessors.php
@@ -14,7 +14,7 @@ use NoOne4rever\Axessors\Exceptions\ReThrownError;
  * Trait Axessors.
  *
  * This trait is used to indicate that a class uses the library and provides them with method call handlers.
- * If you wouldn't use method handling functionality, you can add trait {@link Axessors\Axs}.
+ * If you wouldn't use method handling functionality, you can add trait {@see Axessors\Axs} to your class.
  *
  * @package NoOne4rever\Axessors
  */
@@ -24,9 +24,9 @@ trait Axessors
      * Redirects an instance method call.
      *
      * @param string $method the name of the called method
-     * @param array $args the arguments of the called method
-     * @return mixed return value of the called method
-     * @throws ReThrownError is thrown when there is an error in the eval()'d code
+     * @param array $args arguments of the called method
+     * @return mixed result of the called method
+     * @throws ReThrownError is thrown when there is an error in *injected* callback or condition code
      */
     public function __call(string $method, array $args)
     {
@@ -37,25 +37,24 @@ trait Axessors
      * Redirects a static method call.
      *
      * @param string $method the name of the called method
-     * @param array $args the arguments of the called method
-     * @return mixed return value of the called method
-     * @throws ReThrownError is thrown when there is an error in the eval()'d code
+     * @param array $args arguments of the called method
+     * @return mixed result of the called method
+     * @throws ReThrownError is thrown when there is an error in *injected* callback or condition code
      */
     public static function __callStatic(string $method, array $args)
     {
         return self::__axessorsCall($method, $args);
     }
-    
-    private static function __axessorsCall(string $method, array $args, $object = null)
-    {
-        if (method_exists(static::class, $method)) {
-            return call_user_func_array([$object ?? static::class, $method], $args);
-        } else {
-            $callProcessor = new CallProcessor(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), $object);
-            return $callProcessor->call($args, $method);
-        }
-    }
-    
+
+    /**
+     * Executes *injected* callback or condition.
+     *
+     * @param string $code code to execute
+     * @param mixed $_var value to process
+     * @param bool $mode mode of execution
+     * @return mixed the result or condition or callback
+     * @throws ReThrownError if an error occurred while evaluating *injected* callback or condition
+     */
     public function __axessorsExecute(string $code, $_var, bool $mode)
     {
         $var = $_var;
@@ -68,6 +67,24 @@ trait Axessors
             return $var;
         } else {
             return $mode ? $result : $var;
+        }
+    }
+
+    /**
+     * Redirects Axessors method call.
+     *
+     * @param string $method method name
+     * @param array $args method arguments
+     * @param object|null $object object for instance method call
+     * @return mixed the result of called method
+     */
+    private static function __axessorsCall(string $method, array $args, $object = null)
+    {
+        if (method_exists(static::class, $method)) {
+            return call_user_func_array([$object ?? static::class, $method], $args);
+        } else {
+            $callProcessor = new CallProcessor(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), $object);
+            return $callProcessor->call($args, $method);
         }
     }
 }
