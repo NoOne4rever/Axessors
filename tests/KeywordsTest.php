21de9b60e354e604e0b3bf8752696d27a72e72ce
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-11-14 23:27:53 +0300

    Added PHPdoc comments and tests for setters.

--- tests/KeywordsTest.php
+++ tests/KeywordsTest.php
@@ -13,21 +13,55 @@ use PHPUnit\Framework\TestCase;
 
 require __DIR__ . '/Stubs/KeywordsStub.php';
 
+/**
+ * Class KeywordsTest.
+ *
+ * Tests Axessors keywords.
+ *
+ * @package NoOne4rever\Axessors\Tests
+ */
 class KeywordsTest extends TestCase
 {
-    /** @var KeywordsStub */
+    /** @var KeywordsStub keywords stub */
     private $stub;
 
+    /**
+     * Creates new stub instance.
+     */
     public function setUp()
     {
         $this->stub = new KeywordsStub();
     }
 
+    /**
+     * Tests if automatically generated getters can be called.
+     */
     public function testCreatedGettersCanBeCalled(): void
     {
         $this->assertEquals($this->stub->shortRdb, $this->stub->getShortRdb());
         $this->assertEquals($this->stub->shortAxs, $this->stub->getShortAxs());
         $this->assertEquals($this->stub->fullRdb, $this->stub->getFullRdb());
         $this->assertEquals($this->stub->fullAxs, $this->stub->getFullAxs());
+    }
+
+    /**
+     * Tests if automatically generated setters can be called.
+     */
+    public function testCreatedSettersCanBeCalled(): void
+    {
+        $newShortWrt = 'new short wrt';
+        $newShortAxs = 'new short axs';
+        $newFullWrt = 'new full wrt';
+        $newFullAxs = 'new full axs';
+
+        $this->stub->setShortWrt($newShortWrt);
+        $this->stub->setShortAxs($newShortAxs);
+        $this->stub->setFullWrt($newFullWrt);
+        $this->stub->setFullAxs($newFullAxs);
+
+        $this->assertEquals($newShortWrt, $this->stub->shortWrt);
+        $this->assertEquals($newShortAxs, $this->stub->shortAxs);
+        $this->assertEquals($newFullWrt, $this->stub->fullWrt);
+        $this->assertEquals($newFullAxs, $this->stub->fullAxs);
     }
 }
--- tests/Stubs/KeywordsStub.php
+++ tests/Stubs/KeywordsStub.php
@@ -15,19 +15,29 @@ use NoOne4rever\Axessors\Axessors;
  *
  * @package NoOne4rever\Axessors\Tests\Stubs
  *
- * @method string getShortRdb()
- * @method string getShortAxs()
- * @method string getFullRdb()
- * @method string getFullAxs()
+ * @method string getShortRdb() getter for KeywordsStub::$shortRdb
+ * @method string getShortAxs() getter for KeywordsStub::$shortAxs
+ * @method string getFullRdb() getter for KeywordsStub::$fullRdb
+ * @method string getFullAxs() getter for KeywordsStub::$fullAxs
+ * @method void setShortWrt(string $val) setter for KeywordsStub::$shortWrt
+ * @method void setShortAxs(string $val) setter for KeywordsStub::$shortAxs
+ * @method void setFullWrt(string $val) setter for KeywordsStub::$fullWrt
+ * @method void setFullAxs(string $val) setter for KeywordsStub::$fullAxs
  */
 class KeywordsStub
 {
     use Axessors;
 
+    /** @var string with short rdb keyword */
     public $shortRdb = 'short rdb'; #> +rdb
+    /** @var string with short wrt keyword */
     public $shortWrt = 'short wrt'; #> +wrt
+    /** @var string with short axs keyword */
     public $shortAxs = 'short axs'; #> +axs
+    /** @var string with full rdb keyword */
     public $fullRdb = 'full rdb'; #> +readable
+    /** @var string with full wrt keyword */
     public $fullWrt = 'full wrt'; #> +writable
+    /** @var string with full axs keyword */
     public $fullAxs = 'full axs'; #> +accessible
 }
