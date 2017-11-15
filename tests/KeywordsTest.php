4380037a66f8d23e1430d496886030f1a0047a15
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-11-15 19:17:57 +0300

    Added tests for non-existing methods and setter call without arguments.

--- tests/KeywordsTest.php
+++ tests/KeywordsTest.php
@@ -64,4 +64,43 @@ class KeywordsTest extends TestCase
         $this->assertEquals($newFullWrt, $this->stub->fullWrt);
         $this->assertEquals($newFullAxs, $this->stub->fullAxs);
     }
+
+    /**
+     * Tests if the method, that was not generated, could not be called.
+     *
+     * @dataProvider notGeneratedMethodsProvider
+     * @expectedException \NoOne4rever\Axessors\Exceptions\AxessorsError
+     *
+     * @param string $method method name
+     * @param array $args method arguments
+     */
+    public function testNotGeneratedMethodCouldNotBeCalled(string $method, array $args = []): void
+    {
+        call_user_func_array('call_user_func_array', [[$this->stub, $method], $args]);
+    }
+
+    /**
+     * Tests if an Axessors setter could not be called without arguments.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\AxessorsError
+     */
+    public function testSetterCouldNotBeCalledWithoutArguments(): void
+    {
+        $this->stub->setFullWrt();
+    }
+
+    /**
+     * Provides not generated methods to call.
+     *
+     * @return array methods
+     */
+    public function notGeneratedMethodsProvider(): array
+    {
+        return [
+            'getter for short write-only field' => ['getShortWrt'],
+            'getter for full write-only field' => ['getFullWrt'],
+            'setter for short read-only field' => ['setShortRdb', ['value']],
+            'setter for full read-only field' => ['setFullRdb', ['value']]
+        ];
+    }
 }
