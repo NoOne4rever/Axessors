c57b28d035b6227316dd42385c5a31a4a964b090
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-11-17 16:47:09 +0300

    Added missed class.

---
+++ tests/Stubs/NonAxessorsStub.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is a part of "Axessors" library.
+ *
+ * @author <NoOne4rever@gmail.com>
+ * @package NoOne4rever\Axessors
+ * @license GPL
+ */
+
+namespace NoOne4rever\Axessors\Tests\Stubs;
+
+/**
+ * Class NonAxessorsStub.
+ *
+ * This stub does not belong to code, that is processed by Axessors.
+ *
+ * @package NoOne4rever\Axessors\Tests\Stubs
+ */
+class NonAxessorsStub
+{
+
+}
