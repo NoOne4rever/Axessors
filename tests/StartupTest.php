6b8876aae8da3886d9a837055ac876215ed4e95a
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-11-18 21:58:00 +0300

    Added tests for parse errors.

--- tests/StartupTest.php
+++ tests/StartupTest.php
@@ -8,6 +8,8 @@
 
 namespace NoOne4rever\Axessors\Tests;
 
+use NoOne4rever\Axessors\Axessors;
+use NoOne4rever\Axessors\CommentLexer;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -27,5 +29,68 @@ class StartupTest extends TestCase
         require __DIR__ . '/../src/Startup.php';
 
         $this->assertTrue(true);
+    }
+
+    /**
+     * Tests if invalid default type is processed.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\TypeError
+     */
+    public function testInvalidDefaultTypeFailure(): void
+    {
+        $stub = new class
+        {
+            use Axessors;
+
+            public $field = false; #> +wrt int 
+        };
+
+        $this->indexClass($stub);
+    }
+
+    /**
+     * Tests if null type declaration is processed.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\TypeError
+     */
+    public function testNullTypeDeclarationFailure(): void
+    {
+        $stub = new class
+        {
+            use Axessors;
+
+            public $field; #> +wrt
+        };
+
+        $this->indexClass($stub);
+    }
+
+    /**
+     * Tests if invalid statements order is processed.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\SyntaxError
+     */
+    public function testInvalidStatementsOrder(): void
+    {
+        $stub = new class
+        {
+            use Axessors;
+
+            public $field; #> +rdb int +wrt
+        };
+
+        $this->indexClass($stub);
+    }
+
+    /**
+     * Indexes given classes.
+     *
+     * @param object $object class instance to index
+     */
+    private function indexClass($object): void
+    {
+        $reflection = new \ReflectionClass($object);
+        $lexer = new CommentLexer($reflection);
+        $lexer->getClassData();
     }
 }
