09273f8d7b655b071c00f637e4cbda60e77ba391
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-11-18 20:00:27 +0300

    Added tests for Axessors type declarations.

---
+++ tests/Stubs/TypeDeclarationsStub.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is a part of "Axessors" library.
+ *
+ * @author <NoOne4rever@gmail.com>
+ * @license GPL
+ */
+
+namespace NoOne4rever\Axessors\Tests\Stubs;
+
+use NoOne4rever\Axessors\Axessors;
+
+/**
+ * Class TypeDeclarationsStub.
+ *
+ * @package NoOne4rever\Axessors\Tests\Stubs
+ *
+ * @method void setSingle(string $val) setter for TypeDeclarationsStub::$single
+ * @method void setMultiple(mixed $val) setter for TypeDeclarationsStub::$multiple
+ * @method void setArray(mixed $val) setter for TypeDeclarationsStub::$array
+ * @method void setDefault(mixed $val) setter for TypeDeclarationsStub::$default
+ * @method void setClass(NonAxessorsStub $val) setter for TypeDeclarationsStub::$default
+ * @method void setExtended(array $val) setter for TypeDeclarationsStub::$extended
+ */
+class TypeDeclarationsStub
+{
+    use Axessors;
+
+    /** @var string string field */
+    public $single; #> +wrt string
+    /** @var mixed integer or boolean or string */
+    public $multiple; #> +wrt int|bool|string
+    /** @var string[] array of strings */
+    public $array; #> +wrt array[string]
+    /** @var int field with default type */
+    public $default = 1; #> +wrt int
+    /** @var NonAxessorsStub custom class */
+    public $class; #> +wrt NonAxessorsStub
+    /** @var array extended array */
+    public $extended; #> +wrt Array
+}
---
+++ tests/TypeDeclarationsTest.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * This file is a part of "Axessors" library.
+ *
+ * @author <NoOne4rever@gmail.com>
+ * @license GPL
+ */
+
+namespace NoOne4rever\Axessors\Tests;
+
+use NoOne4rever\Axessors\Tests\Stubs\NonAxessorsStub;
+use NoOne4rever\Axessors\Tests\Stubs\TypeDeclarationsStub;
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/Stubs/TypeDeclarationsStub.php';
+
+/**
+ * Class TypeDeclarationsTest.
+ *
+ * Tests Axessors type declarations.
+ *
+ * @package NoOne4rever\Axessors\Tests
+ */
+class TypeDeclarationsTest extends TestCase
+{
+    /** @var TypeDeclarationsStub type declarations stub */
+    private $stub;
+
+    /**
+     * Creates new stub instance.
+     */
+    public function setUp(): void
+    {
+        $this->stub = new TypeDeclarationsStub();
+    }
+
+    /**
+     * Tests valid data passed to setter can pass type checkout.
+     */
+    public function testValidSingleCanPass(): void
+    {
+        $newValue = 'something';
+
+        $this->stub->setSingle($newValue);
+
+        $this->assertEquals($newValue, $this->stub->single);
+    }
+
+    /**
+     * Tests invalid data passed to setter can not pass type checkout.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\TypeError
+     */
+    public function testInvalidSingleCanNotPass(): void
+    {
+        $newValue = 101;
+
+        $this->stub->setSingle($newValue);
+    }
+
+    /**
+     * Tests valid data passed to setter can pass type checkout.
+     */
+    public function testValidMultipleCanPass(): void
+    {
+        $newValue = 'no';
+        $this->stub->setMultiple($newValue);
+        $this->assertEquals($newValue, $this->stub->multiple);
+
+        $newValue = false;
+        $this->stub->setMultiple($newValue);
+        $this->assertEquals($newValue, $this->stub->multiple);
+
+        $newValue = 0;
+        $this->stub->setMultiple($newValue);
+        $this->assertEquals($newValue, $this->stub->multiple);
+    }
+
+    /**
+     * Tests invalid data passed to setter can not pass type checkout.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\TypeError
+     */
+    public function testInvalidMultipleCanNotPass(): void
+    {
+        $newValue = 256.512;
+        $this->stub->setMultiple($newValue);
+    }
+
+    /**
+     * Tests if valid data can pass typed array checkout.
+     */
+    public function testValidArrayCanPass(): void
+    {
+        $newValue = range('a', 'z');
+
+        $this->stub->setArray($newValue);
+
+        $this->assertEquals($newValue, $this->stub->array);
+    }
+
+    /**
+     * Tests if invalid data can not pass typed array checkout.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\TypeError
+     */
+    public function testInvalidArrayCanNotPass(): void
+    {
+        $newValue = range(1, 10);
+
+        $this->stub->setArray($newValue);
+    }
+
+    /**
+     * Tests if valid data can pass default type checkout.
+     */
+    public function testValidDefaultTypeCanPass(): void
+    {
+        $newValue = 101;
+
+        $this->stub->setDefault($newValue);
+
+        $this->assertEquals($newValue, $this->stub->default);
+    }
+
+    /**
+     * Tests if invalid data can not pass default type checkout.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\TypeError
+     */
+    public function testInvalidDefaultTypeCanNotPass(): void
+    {
+        $newValue = 'smth';
+
+        $this->stub->setDefault($newValue);
+    }
+
+    /**
+     * Tests valid data passed to setter can pass type checkout.
+     */
+    public function testValidClassCanPass(): void
+    {
+        $newValue = new NonAxessorsStub();
+
+        $this->stub->setClass($newValue);
+
+        $this->assertEquals($newValue, $this->stub->class);
+    }
+
+    /**
+     * Tests invalid data passed to setter can not pass type checkout.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\TypeError
+     */
+    public function testInvalidClassCanNotPass(): void
+    {
+        $newValue = new \stdClass();
+
+        $this->stub->setClass($newValue);
+    }
+
+    /**
+     * Tests valid data passed to setter can pass type checkout.
+     */
+    public function testValidExtendedCanPass(): void
+    {
+        $newValue = range(1, 100);
+
+        $this->stub->setExtended($newValue);
+
+        $this->assertEquals($newValue, $this->stub->extended);
+    }
+
+    /**
+     * Tests invalid data passed to setter can not pass type checkout.
+     *
+     * @expectedException \NoOne4rever\Axessors\Exceptions\TypeError
+     */
+    public function testInvalidExtendedCanNotPass(): void
+    {
+        $newValue = 101;
+
+        $this->stub->setExtended($newValue);
+    }
+}
