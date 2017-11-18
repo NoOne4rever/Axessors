26c133ec73c205b43f7a77dd9c719c8723335523
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-11-18 19:07:34 +0300

    Added general Axessors tests.

---
+++ tests/GeneralTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This file is a part of "Axessors" library.
+ *
+ * @author <NoOne4rever@gmail.com>
+ * @license GPL
+ */
+
+namespace NoOne4rever\Axessors\Tests;
+
+use NoOne4rever\Axessors\Tests\Stubs\GeneralStub;
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/Stubs/GeneralStub.php';
+
+/**
+ * Class GeneralTest.
+ *
+ * @package NoOne4rever\Axessors\Tests
+ */
+class GeneralTest extends TestCase
+{
+    /** @var GeneralStub general Axessors stub */
+    private $stub;
+
+    /**
+     * Creates new stub instance.
+     */
+    public function setUp(): void
+    {
+        $this->stub = new GeneralStub();
+    }
+
+    /**
+     * Tests if an instance Axessors method can be called.
+     */
+    public function testInstanceAxessorsMethodCanBeCalled(): void
+    {
+        $this->assertEquals($this->stub->instanceField, $this->stub->getInstanceField());
+    }
+
+    /**
+     * Tests if static Axessors method can be called.
+     */
+    public function testStaticAxessorsMethodCanBeCalled(): void
+    {
+        $this->assertEquals($this->stub::$staticField, $this->stub::getStaticField());
+    }
+
+    /**
+     * Tests if an instance method can be called.
+     */
+    public function testInstanceMethodCanBeCalled(): void
+    {
+        $this->assertEquals('instance method', $this->stub->testInstance());
+    }
+
+    /**
+     * Tests if static method can be called.
+     */
+    public function testStaticMethodCanBeCalled(): void
+    {
+        $this->assertEquals('static method', $this->stub::testStatic());
+    }
+
+    /**
+     * Tests if a natural method has higher priority than an Axessors method.
+     */
+    public function testNaturalMethodHasHigherPriorityThanAxessorsMethod(): void
+    {
+        $this->assertNotEquals($this->stub->field, $this->stub->getField());
+    }
+}
---
+++ tests/Stubs/GeneralStub.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This file is a part of "Axessors" library.
+ *
+ * @author <NoOne4rever@gmail.com>
+ * @license GPL
+ */
+
+namespace NoOne4rever\Axessors\Tests\Stubs;
+
+use NoOne4rever\Axessors\Axessors;
+
+/**
+ * Class GeneralStub.
+ *
+ * @package NoOne4rever\Axessors\Tests\Stubs
+ *
+ * @method string getInstanceField() getter for GeneralStub::$instanceField
+ * @method static string getStaticField() getter for GeneralStub::$staticField
+ */
+class GeneralStub
+{
+    use Axessors;
+
+    /** @var string static field with Axessors */
+    public static $staticField = 'static'; #> +rdb
+    /** @var string instance field with Axessors */
+    public $instanceField = 'instance'; #> +rdb
+    public $field = 'field value'; #> +rdb
+
+    /**
+     * Tests static method call.
+     *
+     * @return string result
+     */
+    public static function testStatic(): string
+    {
+        return 'static method';
+    }
+
+    /**
+     * Tests instance method call.
+     *
+     * @return string result
+     */
+    public function testInstance(): string
+    {
+        return 'instance method';
+    }
+
+    /**
+     * Tests general method.
+     *
+     * @return string result
+     */
+    public function getField(): string
+    {
+        return 'method result';
+    }
+}
