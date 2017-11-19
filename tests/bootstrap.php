4f2b7a9447324e39bf67d3ce2c8987bf3c7e92b2
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-11-19 14:56:07 +0300

    Added tests bootstrap.

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file is a part of "Axessors" library.
+ *
+ * @author <NoOne4rever@gmail.com>
+ * @license GPL
+ */
+
+namespace NoOne4rever\Axessors\Tests;
+
+require __DIR__ . '/AxessorsTestCase.php';
+require __DIR__ . '/../vendor/autoload.php';
