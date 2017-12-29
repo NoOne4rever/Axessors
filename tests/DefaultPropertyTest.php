46aca10e2b0bd32d9e158fbb95826d1a1076028c
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-12-29 20:19:17 +0300

    Improved tests.

--- tests/DefaultPropertyTest.php
+++ tests/DefaultPropertyTest.php
@@ -1,16 +1,46 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: NoOne
- * Date: 29.12.2017
- * Time: 19:56
+ * This file is a part of "Axessors" library.
+ *
+ * @author <NoOne4rever@gmail.com>
+ * @license GPL
  */
 
 namespace NoOne4rever\Axessors\Tests;
 
-use PHPUnit\Framework\TestCase;
+use NoOne4rever\Axessors\Tests\Stubs\DefaultPropertyStub;
 
-class DefaultPropertyTest extends TestCase
+require __DIR__ . '/Stubs/DefaultPropertyStub.php';
+
+class DefaultPropertyTest extends AxessorsTestCase
 {
+    /**
+     * Creates new stub instance.
+     */
+    public function setUp(): void
+    {
+        $this->stub = new DefaultPropertyStub();
+    }
 
+    /**
+     * Tests default getter.
+     */
+    public function testDefaultGetter(): void
+    {
+        $this->stub->def = 256;
+        
+        $this->assertEquals($this->stub->def, $this->stub->get());
+    }
+
+    /**
+     * Tests default setter.
+     */
+    public function testDefaultSetter(): void
+    {
+        $value = 256;
+        
+        $this->stub->set($value);
+        
+        $this->assertEquals($value, $this->stub->def);
+    }
 }
--- tests/Stubs/DefaultPropertyStub.php
+++ tests/Stubs/DefaultPropertyStub.php
@@ -1,15 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: NoOne
- * Date: 29.12.2017
- * Time: 19:55
+ * This file is a part of "Axessors" library.
+ *
+ * @author <NoOne4rever@gmail.com>
+ * @license GPL
  */
 
 namespace NoOne4rever\Axessors\Tests\Stubs;
 
+use NoOne4rever\Axessors\Axessors;
 
+/**
+ * Class DefaultPropertyStub.
+ * 
+ * @package NoOne4rever\Axessors\Tests\Stubs
+ */
 class DefaultPropertyStub
 {
-
+    use Axessors;
+    
+    public $def; #: +axs int => default
 }
