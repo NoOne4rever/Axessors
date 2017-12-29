bc1bf1821e4849088cda8c0ddf01e40a052febb3
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-12-29 20:03:07 +0300

    Added tests for default properties.

---
+++ tests/DefaultPropertyTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NoOne
+ * Date: 29.12.2017
+ * Time: 19:56
+ */
+
+namespace NoOne4rever\Axessors\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DefaultPropertyTest extends TestCase
+{
+
+}
---
+++ tests/Stubs/DefaultPropertyStub.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NoOne
+ * Date: 29.12.2017
+ * Time: 19:55
+ */
+
+namespace NoOne4rever\Axessors\Tests\Stubs;
+
+
+class DefaultPropertyStub
+{
+
+}
