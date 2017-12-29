4679230b520c801301d439ba9e7d61a8af54939b
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-12-29 14:50:16 +0300

    Added tests for short "$." syntax.

---
+++ tests/InjectedStringsTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NoOne
+ * Date: 29.12.2017
+ * Time: 14:38
+ */
+
+namespace NoOne4rever\Axessors\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class InjectedStringsTest extends TestCase
+{
+
+}
---
+++ tests/Stubs/InjectedStringStub.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NoOne
+ * Date: 29.12.2017
+ * Time: 14:36
+ */
+
+namespace NoOne4rever\Axessors\Tests\Stubs;
+
+
+class InjectedStringStub
+{
+
+}
