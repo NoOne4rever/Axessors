b73eb461982548da706240ca63e8069c0d1ac43a
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-12-29 14:52:23 +0300

    Added test methods for short "$." syntax.

--- tests/Stubs/InjectedStringStub.php
+++ tests/Stubs/InjectedStringStub.php
@@ -1,15 +1,24 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: NoOne
- * Date: 29.12.2017
- * Time: 14:36
+ * This file is a part of "Axessors" library.
+ *
+ * @author <NoOne4rever@gmail.com>
+ * @license GPL
  */
 
 namespace NoOne4rever\Axessors\Tests\Stubs;
 
+use NoOne4rever\Axessors\Axessors;
 
+/**
+ * Stub for injected strings.
+ * 
+ * @package NoOne4rever\Axessors\Tests\Stubs
+ */
 class InjectedStringStub
 {
-
+    use Axessors;
+    
+    public $x;
+    public $thisUsage; #: +axs int `isset($.x)`
 }
