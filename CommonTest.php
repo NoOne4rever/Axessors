968d7355999a332bdbc37cd8ebb4e5d4319c5602
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-12-30 21:11:23 +0300

    Added simple test for grouped conditions.

--- CommonTest.php
+++ CommonTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * This file is a part of "Axessors" library.
- *
- * @author <NoOne4rever@gmail.com>
- * @license GPL
- */
 
 namespace NoOne4rever\Axessors\Tests;
 
@@ -13,48 +7,15 @@ use NoOne4rever\Axessors\AxessorsStartup;
 
 require __DIR__ . '/vendor/autoload.php';
 
-/**
- * Class Color.
- * 
- * @package NoOne4rever\Axessors\Tests
- */
-class Color
+class TestCase
 {
     use Axessors;
     
-    private $red, $green, $blue; #: +axs int `{$a = 0; $b = 255; return $var >= $a && $var <= $b;}`
-    
-    public function __construct(int $red, int $green, int $blue)
-    {
-        $this->setRed($red);
-        $this->setGreen($green);
-        $this->setBlue($blue);
-    }
-    
-    public function mix(Color $color): Color
-    {
-        $red = $this->avg($this->red, $color->red);
-        $green = $this->avg($this->green, $color->green);
-        $blue = $this->avg($this->blue, $color->blue);
-        
-        return new Color($red, $green, $blue);
-    }
-    
-    private function avg(int $a, int $b): float
-    {
-        return ($a + $b) / 2;
-    }
+    private $field; #: +axs int (!= 4 && (1..10 || == 100))
 }
 
 AxessorsStartup::run();
 
-$cyan = new Color(0, 255, 255);
-$black = new Color(0, 0, 0);
-
-$mixed = $cyan->mix($black);
-
-echo <<<TXT
-red: {$mixed->getRed()};
-green: {$mixed->getGreen()};
-blue: {$mixed->getBlue()};
-TXT;
+$test = new TestCase();
+$test->setField(100);
+$test->getField();
