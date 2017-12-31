6299b29260900ca565e19056f32f26b03298f622
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2017-12-31 12:58:25 +0300

    Removed unused methods.

--- src/ConditionsRunner.php
+++ src/ConditionsRunner.php
@@ -12,9 +12,9 @@ use NoOne4rever\Axessors\Exceptions\TypeError;
 
 /**
  * Class ConditionsSuit.
- * 
+ *
  * Processes Axessors conditions.
- * 
+ *
  * @package NoOne4rever\Axessors
  */
 class ConditionsRunner extends RunningSuit
@@ -24,7 +24,7 @@ class ConditionsRunner extends RunningSuit
 
     /**
      * ConditionsSuit constructor.
-     * 
+     *
      * @param int $mode mode of execution
      * @param PropertyData $data property data
      * @param string $class class
@@ -35,100 +35,6 @@ class ConditionsRunner extends RunningSuit
     {
         parent::__construct($mode, $data, $class, $object);
         $this->method = $method;
-    }
-
-    /**
-     * Checks the conditions defined in the Axessors comment.
-     *
-     * Creates logical tree of the conditions and then checks if the general result is true.
-     *
-     * @param $var mixed value of the property
-     * @return bool result of checking of the conditions
-     */
-    public function processConditions($var): bool
-    {
-        if ($this->mode == RunningSuit::INPUT_MODE) {
-            $conditions = $this->propertyData->getInputConditions();
-        } else {
-            $conditions = $this->propertyData->getOutputConditions();
-        }
-        return $this->executeInjectedString($conditions, $var, $this->mode);
-    }
-
-    /**
-     * Reduces conditions tree and returns the result.
-     * 
-     * @param mixed $carry previous item
-     * @param mixed $item item
-     * @param bool $mode mode
-     * @return bool the result of reducing
-     */
-    private function reduce($carry, $item, bool $mode): bool 
-    {
-        if ($item === $mode || $carry === $mode) {
-            return $mode;
-        } elseif (is_array($item)) {
-            return array_reduce($item, function ($carry, $item) {
-                return $this->reduce($carry, $item, false);
-            });
-        } else {
-            return !$mode;
-        }
-    }
-
-    /**
-     * Calculates every condition defined in the Axessors comment.
-     *
-     * @param $value mixed value of the property
-     * @return bool[] results of the conditions
-     */
-    private function calculateConditions($value): array
-    {
-        $calculatedConditions = [];
-        $conditions = $this->mode == RunningSuit::OUTPUT_MODE ? $this->propertyData->getOutputConditions() : $this->propertyData->getInputConditions();
-        foreach ($conditions as $number => $complexCondition) {
-            if (is_array($complexCondition)) {
-                foreach ($complexCondition as $condition) {
-                    $calculatedConditions[$number][] = $this->executeCondition($condition, $value);
-                }
-            } else {
-                $calculatedConditions[$number] = $this->executeCondition($complexCondition, $value);
-            }
-        }
-        return $calculatedConditions;
-    }
-
-    /**
-     * Calculates a condition defined in the Axessors comment.
-     *
-     * @param $condition string the condition
-     * @param $value mixed value of the property
-     * @return bool result of the condition
-     */
-    private function executeCondition(string $condition, $value): bool
-    {
-        if (strpos($condition, '`') !== false) {
-            return $this->executeInjectedString($condition, $value, true);
-        } else {
-            return $this->runStandardCondition($condition, $value);
-        }
-    }
-
-    /**
-     * Runs Axessors condition.
-     * 
-     * @param string $condition condition
-     * @param mixed $value value to check 
-     * @return bool the result of the checkout
-     */
-    private function runStandardCondition(string $condition, $value): bool 
-    {
-        $value = $this->count($value);
-        if (strpos($condition, '..') !== false) {
-            $condition = explode('..', $condition);
-            $condition = "<= {$condition[1]} && $value >= {$condition[0]}";
-        }
-        return eval("return $value $condition;");
     }
 
     /**
@@ -154,8 +60,43 @@ class ConditionsRunner extends RunningSuit
                 $value = count($value);
                 break;
             default:
-                throw new TypeError('value is not countable');
+                throw new TypeError('value of ' . var_export($value, true) . ' is not countable');
         }
         return $value;
+    }
+
+    /**
+     * Checks the conditions defined in the Axessors comment.
+     *
+     * Creates logical tree of the conditions and then checks if the general result is true.
+     *
+     * @param $var mixed value of the property
+     * @return bool result of checking of the conditions
+     */
+    public function processConditions($var): bool
+    {
+        if ($this->mode == RunningSuit::INPUT_MODE) {
+            $conditions = $this->propertyData->getInputConditions();
+        } else {
+            $conditions = $this->propertyData->getOutputConditions();
+        }
+        return $this->executeInjectedString($conditions, $var, $this->mode);
+    }
+
+    /**
+     * Executes conditions.
+     * 
+     * @param string $expr conditions expression
+     * @param mixed $var value to process
+     * @param bool $mode mode of execution defined in RunningSuit
+     * @return bool the result of condition
+     */
+    protected function executeInjectedString(string $expr, $var, bool $mode): bool 
+    {
+        if (is_null($this->object)) {
+            return call_user_func("{$this->class}::__axessorsExecute", $expr, $var, $mode);
+        } else {
+            return $this->object->__axessorsExecute($expr, $var, $mode);
+        }
     }
 }
