23794018edc04adcd782afef231b2957dad7d2c8
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2018-01-06 01:22:25 +0300

    CommonTest excluded from Scrutinizer CI analysis.

--- CommonTest.php
+++ CommonTest.php
@@ -1,21 +1,50 @@
 <?php
 
-namespace NoOne4rever\Axessors\Tests;
-
-use NoOne4rever\Axessors\Axessors;
-use NoOne4rever\Axessors\AxessorsStartup;
+use NoOne4rever\Axessors\{
+    Axessors, AxessorsStartup, Exceptions\AxessorsError
+};
 
 require __DIR__ . '/Axessors.phar';
 
-class TestCase
+class Point
 {
     use Axessors;
-    
-    private $field; #: +axs int (!= 4 && (1..10 || == 100) && `{return 'ok';}`)
+
+    private $x, $y; #: +axs int
+
+    public function Point(int $x, int $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+}
+
+class Line
+{
+    use Axessors;
+
+    private $start; #: +axs Point
+    private $end; #: +axs Point => default
+
+    public function Line(Point $start, Point $end)
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
 }
 
 AxessorsStartup::run();
 
-$test = new TestCase();
-$test->setField(100);
-echo $test->getField();
+$start = new Point(10, 10);
+$end = new Point(20, 20);
+$line = new Line($start, $end);
+
+$line->getStart()->setX(15);
+echo $line->getStart()->getX(), PHP_EOL;
+
+$line->get();
+try {
+    $line->getEnd();
+} catch (AxessorsError $error) {
+    echo $error->getMessage();
+}
