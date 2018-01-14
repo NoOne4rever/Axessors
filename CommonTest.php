45a12ac4b05229f0331b2c1da3b17f2b72d9823f
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2018-01-14 17:09:59 +0300

    Changed to example of Axessors methods for default fields.

--- CommonTest.php
+++ CommonTest.php
@@ -1,50 +1,23 @@
 <?php
 
+namespace NoOne4rever\Axessors\Tests;
+
 use NoOne4rever\Axessors\{
-    Axessors, AxessorsStartup, Exceptions\AxessorsError
+    Axessors,
+    AxessorsStartup
 };
 
-require __DIR__ . '/Axessors.phar';
+require __DIR__ . '/vendor/autoload.php';
 
-class Point
+class CommonTest
 {
     use Axessors;
 
-    private $x, $y; #: +axs int
-
-    public function Point(int $x, int $y)
-    {
-        $this->x = $x;
-        $this->y = $y;
-    }
-}
-
-class Line
-{
-    use Axessors;
-
-    private $start; #: +axs Point
-    private $end; #: +axs Point => default
-
-    public function Line(Point $start, Point $end)
-    {
-        $this->start = $start;
-        $this->end = $end;
-    }
+    private $field = 0; #: +axs int => default
 }
 
 AxessorsStartup::run();
 
-$start = new Point(10, 10);
-$end = new Point(20, 20);
-$line = new Line($start, $end);
-
-$line->getStart()->setX(15);
-echo $line->getStart()->getX(), PHP_EOL;
-
-$line->get();
-try {
-    $line->getEnd();
-} catch (AxessorsError $error) {
-    echo $error->getMessage();
-}
+$test = new CommonTest();
+$test->increment();
+echo $test->get();
